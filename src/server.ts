b92d8d54293e99a66fc66b8a7d231bfe27878df6
John Wee <91670254+johnwee1@users.noreply.github.com>
2024-12-16 22:21:53 +0800

    change ports

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ const io = new Server(httpServer, {
 
 initializeSocket(io);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 10000;
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
